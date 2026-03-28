863cd4f42c9d3a29ce4d64e56b8af2f5717f5a1b
i8DishantSingh <professional.id.dishant@gmail.com>
2026-03-28 09:53:43 +0530

    Configured Database with server

---
+++ backend/config/db.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import { ENV_VARS } from "./envVars.js";
+
+
+export const connectDB = async () => {
+
+    try {
+        const conn = await mongoose.connect(ENV_VARS.MONGO_URI)
+        console.log(`MongoDB connected: ${conn.connection.host}`);
+    } catch (error) {
+        console.error(`Error connecting to Database: ${error.message}`);
+        process.exit(1);  
+    }
+}
+
+
