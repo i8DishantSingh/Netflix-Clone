595f07168e3da85df7aa664ee29eecf1323f346e
i8DishantSingh <professional.id.dishant@gmail.com>
2026-04-03 15:41:34 +0530

    Created auth singup logic

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,10 +1,66 @@
+import { User } from "../models/user.model.js";
+import bcryptjs from 'bcryptjs';
+
 
 export async function signup(req, res) {
     
     try {
+        const { username, email, password, image } = req.body;
         
+        if (!username || !email || !password) {
+            return res.status(400).json({succes: false, message : 'All fields are required' });
+        }
+
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+        if (!emailRegex.test(email)) {
+            return res.status(400).json({ succes: false, message : 'Invalid email format' });
+        }
+
+        if (password.length < 8) {
+            return res.status(400).json({ succes: false, message : 'Password must be at least 8 characters long'})
+        }
+
+        const existingUserByEmail = await User.findOne({email: email})
+
+        if (existingUserByEmail) {
+            return res.status(400).json({ succes: false, message : 'Email already in use'});
+        }
+
+        const existingUserByUsername = await User.findOne({username: username})
+
+        if (existingUserByUsername) {
+            return res.status(400).json({ succes: false, message : 'Username already exists'});
+        }
+        
+        const salt = await bcryptjs.genSalt(10);
+        const hashedPassword = await bcryptjs.hash(password, salt);
+
+
+
+        const newUser = new User({
+            username: username,
+            email: email, 
+            password: hashedPassword,
+            image: image
+        })
+
+        await newUser.save();
+        res.status(201).json({
+          sucess: true,
+          message: "User created successfully",
+          user: {
+            ...newUser._doc,
+            password: "********",
+          },
+        });
+
     } catch (error) {
-        
+        console.error('Error during signup: ', error.message);
+        res.status(500).json({
+            success: false,
+            message : 'Internal server error',
+            });
     }
 }
 
