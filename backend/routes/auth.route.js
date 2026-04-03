8a4a40381e5d38a225423b8b0fe9bc5f8631aa65
i8DishantSingh <professional.id.dishant@gmail.com>
2026-04-03 15:42:44 +0530

    organised routes

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -3,9 +3,13 @@ import { signup, lognin, logout } from '../controllers/auth.controller.js';
     
 const router = express.Router();
 
-router.get('/signup', signup);
-router.get('/login', lognin);
-router.get('/logout', logout);
+router.get('/signup', (req, res) => {
+    res.send('Signup route');
+})
+
+router.post('/signup', signup);
+router.post('/login', lognin);
+router.post('/logout', logout);
 
 
 export default router;
